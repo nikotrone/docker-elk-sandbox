8f11a5ae5b9d657a881f0aa6612877de5c2ac68b
Marco Nicotra <nikotrone@gmail.com>
2021-03-04 11:57:07 +0100

    First draft

---
+++ generate_test_data.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+YEAR=2020
+
+for MONTH in {1..12}; 
+do 
+    for DAY in {1..28};
+    do
+        curl -XPUT "http://localhost:9200/testdata-$YEAR-$MONTH-$DAY/test/message" -H 'Content-Type: application/json' -d '{
+ "answer": 42,
+ "message": "this is my test data"
+}'
+        echo "$day-$month";
+    done 
+done
